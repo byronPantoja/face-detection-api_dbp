9d7c5dd64478daea0f7412a77e0f573b80d71fd4
dev833 <byronpantoja@gmail.com>
2020-05-01 11:01:20 +0800

    adding a jsconfig file, bcrypt, and cors

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors')
 
 const app = express();
 
-app.use(bodyParser.json());
 const database = {
 	users: [
 		{
@@ -23,10 +24,21 @@ const database = {
 			joined: new Date(),
 		},
 	],
+	login: [
+		{
+			id: '987',
+			hash: '',
+			email: 'don@gmoe.com',
+		},
+	],
 };
 
+
+app.use(bodyParser.json());
+app.use(cors())
+
 app.get('/', (req, res) => {
-	res.send('This is wired up.');
+	res.send(database.users);
 });
 
 app.post('/signin', (req, res) => {
@@ -40,8 +52,62 @@ app.post('/signin', (req, res) => {
 	}
 });
 
-app.listen(5000, () => {
-	console.log('We are listening on port 5000');
+app.post('/register', (req, res) => {
+	const { email, name, password } = req.body;
+	bcrypt.hash(password, null, null, function(err, hash) {
+		console.log(hash);
+	});
+	database.users.push({
+		id: '003',
+		name: name,
+		email: email,
+		password: password,
+		entries: 0,
+		joined: new Date(),
+	});
+	res.json(database.users[database.users.length - 1]); //length -1 grabs the last item in the array
+});
+
+app.get('/profile/:id', (req, res) => {
+	const { id } = req.params;
+	let found = false;
+	database.users.forEach((user) => {
+		if (user.id === id) {
+			found = true;
+			return res.json(user);
+		}
+	});
+	if (!found) {
+		res.status(400).json('not found');
+	}
+});
+
+app.post('/image', (req, res) => {
+	const { id } = req.body;
+	let found = false;
+	database.users.forEach((user) => {
+		if (user.id === id) {
+			found = true;
+			user.entries++;
+			return res.json(user.entries);
+		}
+	});
+	if (!found) {
+		res.status(400).json('not found');
+	}
+});
+
+
+// // Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function(err, res) {
+//   // res == true
+// });
+// bcrypt.compare("veggies", hash, function(err, res) {
+//   // res = false
+// });
+
+app.listen(3000, () => {
+	console.log('We are listening on port 3000');
 });
 
 /**
