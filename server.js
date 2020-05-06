ded6958e9369c4fa4be205418f3ba247feb5a7de
dev833 <byronpantoja@gmail.com>
2020-05-06 08:53:50 +0800

    Adding bcrypt to the db and wiring it all up - finishing up this review project.

--- server.js
+++ server.js
@@ -54,29 +54,52 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
-	if (
-		req.body.email === database.users[0].email &&
-		req.body.password === database.users[0].password
-	) {
-		res.json(database.users[0]);
-	} else {
-		res.status(400).json('Error logging in.');
-	}
+	db.select('email', 'hash')
+		.from('login')
+		.where('email', '=', req.body.email)
+		.then((data) => {
+			const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+			if (isValid) {
+				db.select('*')
+					.from('users')
+					.where('email', '=', req.body.email)
+					.then((user) => {
+						res.json(user[0]);
+						//console.log(user);
+					})
+					.catch((err) => res.status(400).json('Unable to get user'));
+			} else {
+				res.status(400).json('Wrong Credentials');
+			}
+		});
 });
 
 app.post('/signup', (req, res) => {
 	const { email, name, password } = req.body;
-	db('users')
-		.returning('*')
-		.insert({
-			name: name,
-			email: email,
-			joined: new Date(),
-		})
-		.then((user) => {
-			res.json(user[0]);
-		})
-		.catch((err) => res.status(400).json(err)); //From knex documentation under Clear Methods: -insert
+	const hash = bcrypt.hashSync(password);
+	db.transaction((trx) => {
+		trx
+			.insert({
+				hash: hash,
+				email: email,
+			})
+			.into('login')
+			.returning('email')
+			.then((loginEmail) => {
+				return trx('users')
+					.returning('*')
+					.insert({
+						email: loginEmail[0],
+						name: name,
+						joined: new Date(),
+					})
+					.then((user) => {
+						res.json(user[0]);
+					});
+			})
+			.then(trx.commit)
+			.catch(trx.rollback);
+	}).catch((err) => res.status(400).json('Unable to Register'));
 });
 
 app.get('/profile/:id', (req, res) => {
